420f15a4a7667fef0a51a08ac7e84c5714465155
An Orbit <68935009+orbit-loona@users.noreply.github.com>
2024-01-26 00:56:01 -0500

    Use a prompt to make the value changeable when selecting the element

--- mods/tempstuff.js
+++ mods/tempstuff.js
@@ -1,9 +1,21 @@
-var temptouse = 0
-elements.customtemp = {
+tempToUse = 0;
+
+elements.custom_temp = {
     color: ["#ff0000", "#ffbf4f", "#ff0000", "#4fbfff", "#0000ff", "#4fbfff"],
     tool: function(pixel) {
-    pixel.temp = temptouse
-		pixelTempCheck(pixel)
+        pixel.temp = tempToUse;
+        pixelTempCheck(pixel)
+    },
+    onSelect: function() {
+        var value = prompt("Enter the temperature you want to use");
+        //if the user backs out of the prompt (value = null) or just types nothing (value = ""), don't change the temperature to 0 (Number turns both into 0)
+        if([null,""].includes(value)) { return };
+        value = Number(value);
+        if(isNaN(value)) {
+            alert("Temperature was invalid! Please enter a number.");
+            return
+        };
+        tempToUse = value
     },
     category: "tools",
 };
